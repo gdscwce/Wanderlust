c860be950172fa11fa86b17c6ede93c166a40071
kshitij <kshitijmjadhav@gmail.com>
2021-09-24 15:47:45 +0530

    Hello

---
+++ app/src/main/java/com/example/wanderlust/Login_Activity.java
@@ -0,0 +1,13 @@
+package com.example.wanderlust;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+    }
+}
--- app/src/main/java/com/example/wanderlust/SplashActivity.java
+++ app/src/main/java/com/example/wanderlust/SplashActivity.java
@@ -1,15 +1,25 @@
 package com.example.wanderlust;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView bg, logo;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        bg = findViewById(R.id.background);
+        logo = findViewById(R.id.logo);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
@@ -18,18 +28,29 @@ public class SplashActivity extends AppCompatActivity {
         
         runNextScreen();
 
+
+
     }
 
     private void runNextScreen() {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                Intent intent = new Intent(SplashActivity.this, StartActivity.class);
                 startActivity(intent);
                 finish();
+
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
             }
         }, 1500);
+    }
+
+    public void Login_Activity(View view) {
+
+
+
+
+
     }
 }
